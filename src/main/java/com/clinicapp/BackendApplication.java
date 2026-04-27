ee14766504a02d54d89ecb04bd1d4d818a4f47aa
Luis De la sota <luis.mdelasota@gmail.com>
2026-04-26 21:22:58 -0500

    Proyecto inicial con configuracion segura y dependencias

---
+++ src/main/java/com/clinicapp/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.clinicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+
+        //Arrancar Spring
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
